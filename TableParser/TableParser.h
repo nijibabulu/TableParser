d3b70480cce38b59a23caad3057257d7ffeb5dc2
Bob Zimmermann <robert.zimmermann@univie.ac.at>
2017-05-23 21:30:30 +0200

    Initial Commit

---
+++ TableParser/TableParser.h
@@ -0,0 +1,19 @@
+//
+//  TableParser.h
+//  TableParser
+//
+//  Created by Bob on 23/05/2017.
+//  Copyright © 2017 Zimmerapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TableParser.
+FOUNDATION_EXPORT double TableParserVersionNumber;
+
+//! Project version string for TableParser.
+FOUNDATION_EXPORT const unsigned char TableParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TableParser/PublicHeader.h>
+
+
